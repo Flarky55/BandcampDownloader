67be0bcd9afdfc186bfa29808948ec4b20882312
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2025-01-31 17:31:14 +0600

    Changed constant links to repository

--- src/BandcampDownloader/Core/Constants.cs
+++ src/BandcampDownloader/Core/Constants.cs
@@ -20,31 +20,31 @@ namespace BandcampDownloader
         /// <summary>
         /// The URL redirecting to the changelog file on GitHub.
         /// </summary>
-        public static readonly string UrlChangelog = "https://raw.githubusercontent.com/Otiel/BandcampDownloader/master/CHANGELOG.md";
+        public static readonly string UrlChangelog = "https://raw.githubusercontent.com/Flarky55/BandcampDownloader/master/CHANGELOG.md";
         /// <summary>
         /// The URL redirecting to the help page on translating the app on GitHub.
         /// </summary>
-        public static readonly string UrlHelpTranslate = "https://github.com/Otiel/BandcampDownloader/blob/master/docs/help-translate.md";
+        public static readonly string UrlHelpTranslate = "https://github.com/Flarky55/BandcampDownloader/blob/master/docs/help-translate.md";
         /// <summary>
         /// The URL redirecting to the issues page on GitHub.
         /// </summary>
-        public static readonly string UrlIssues = "https://github.com/Otiel/BandcampDownloader/issues";
+        public static readonly string UrlIssues = "https://github.com/Flarky55/BandcampDownloader/issues";
         /// <summary>
         /// The URL redirecting to the latest release on GitHub.
         /// </summary>
-        public static readonly string UrlLatestRelease = "https://github.com/Otiel/BandcampDownloader/releases/latest";
+        public static readonly string UrlLatestRelease = "https://github.com/Flarky55/BandcampDownloader/releases/latest";
         /// <summary>
         /// The URL redirecting to the releases page on GitHub.
         /// </summary>
-        public static readonly string UrlReleases = "https://github.com/Otiel/BandcampDownloader/releases";
+        public static readonly string UrlReleases = "https://github.com/Flarky55/BandcampDownloader/releases";
         /// <summary>
         /// The URL redirecting to the zip file. Must be formatted with a version.
         /// </summary>
-        public static readonly string UrlReleaseZip = "https://github.com/Otiel/BandcampDownloader/releases/download/v{0}/BandcampDownloader.zip";
+        public static readonly string UrlReleaseZip = "https://github.com/Flarky55/BandcampDownloader/releases/download/v{0}/BandcampDownloader.zip";
         /// <summary>
         /// The website URL of BandcampDownloader.
         /// </summary>
-        public static readonly string UrlWebsite = "https://github.com/Otiel/BandcampDownloader";
+        public static readonly string UrlWebsite = "https://github.com/Flarky55/BandcampDownloader";
         /// <summary>
         /// The absolute path to the settings file.
         /// </summary>
